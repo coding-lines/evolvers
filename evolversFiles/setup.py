e70f73c52432fe9c176156941a9f57508e06a016
Jannik Müller <jannik30013@gmail.com>
2020-04-14 21:41:20 +0200

    Updated build file

--- evolversFiles/setup.py
+++ evolversFiles/setup.py
@@ -2,4 +2,4 @@ import cx_Freeze
 
 executables = [cx_Freeze.Executable("runGame_GUI.py",icon="icon.ico",base="Win32GUI")]
 
-cx_Freeze.setup(name="Evolvers",options={"build_exe":{"packages":["pygame","random"],"include_files":["menuBackground.jpg","true.png","false.png","icon.png","logoSmall.png","creatureEngine.py","newWorldGenerator.py","mouseHoverEngine.py","cameraEngine.py","creatureClickEngine.py","creatureNames.py","font_pt-sans.ttf"]}},executables=executables)
+cx_Freeze.setup(name="Evolvers",options={"build_exe":{"worldImage.jpg","pygame_textinput.py","left.png","right.png","packages":["pygame","random"],"include_files":["menuBackground.jpg","true.png","false.png","icon.png","logoSmall.png","creatureEngine.py","newWorldGenerator.py","mouseHoverEngine.py","cameraEngine.py","creatureClickEngine.py","creatureNames.py","font_pt-sans.ttf"]}},executables=executables)
