15acb7e2d12f8e52f7ed7398c524447c5bef9619
Jannik <53686562+evolving-dev@users.noreply.github.com>
2023-08-09 16:36:15 +0200

    Removed Evolvers v1 codebase

--- Evolvers/creatureClickEngine.py
+++
@@ -1,7 +0,0 @@
-def assignClick(mousePos,cameraPos,creatures,sidebarOpen,scale):
-    if not sidebarOpen or mousePos[0] < 1000:
-        ingameCoords = [(mousePos[0] // scale) + cameraPos[0], (mousePos[1] // scale)+cameraPos[1]]
-        for creature in range(len(creatures)):
-            if int(creatures[creature]["x"]) in range(ingameCoords[0]-2,ingameCoords[0]+2) and int(creatures[creature]["y"]) in range(ingameCoords[1]-2,ingameCoords[1] +2):
-                return creature
-        
